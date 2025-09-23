8dbf8acbcca8db926618b80067a0182962b64b2d
Adair Sosa <jesussosa9859@gmail.com>
2025-09-23 17:06:31 -0600

    fix color

--- src/components/technologies/technnologies.jsx
+++ src/components/technologies/technnologies.jsx
@@ -6,35 +6,35 @@ const tech = () => {
            </div>
             <div id="section-aboutMe" class='max-w-screen-xl flex flex-col lg:flex-row justify-center items-center lg:items-center p-5 pt-5 mx-auto gap-5'>
                 <div class='grid  sm:grid-cols-1 md:grid col-end-3 gap-3 lg:grid-cols-4'>
-                    <div class="flex bg-blue-600 dark:bg-yellow-400 rounded-xl pr-4 pl-4 ">
+                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4 ">
                         <img class="h-auto max-w-auto rounded-lg" src="https://img.icons8.com/?size=100&id=90519&format=png&color=000000" alt=""/>
                         <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">Spring Boot</span>
                     </div>
-                    <div class="flex bg-blue-600 dark:bg-yellow-400 rounded-xl pr-4 pl-4">
+                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
                         <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=38561&format=png&color=000000" alt=""/>
                         <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PostgreSQL</span>
                     </div>
-                    <div class="flex bg-blue-600 dark:bg-yellow-400 rounded-xl pr-4 pl-4">
+                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
                         <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=cdYUlRaag9G9&format=png&color=000000" alt=""/>
                         <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Docker</span>
                     </div>
-                    <div class="flex bg-blue-600 dark:bg-yellow-400 rounded-xl pr-4 pl-4">
+                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
                         <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=WHRLQdbEXQ16&format=png&color=000000" alt=""/>
                         <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GCP</span>
                     </div>
-                    <div class="flex bg-blue-600 dark:bg-yellow-400 rounded-xl pr-4 pl-4">
+                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
                         <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=39858&format=png&color=0D5AA1" alt=""/>
                         <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MySQL</span>
                     </div>
-                    <div class="flex bg-blue-600 dark:bg-yellow-400 rounded-xl pr-4 pl-4">
+                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
                         <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=l9a5tcSnBwcf&format=png&color=F44336" alt=""/>
                         <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Angular</span>
                     </div>
-                    <div class="flex bg-blue-600 dark:bg-yellow-400 rounded-xl pr-4 pl-4">
+                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
                         <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=GPfHz0SM85FX&format=png&color=000000" alt=""/>
                         <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Java</span>
                     </div>
-                    <div class="flex bg-blue-600 dark:bg-yellow-400 rounded-xl pr-4 pl-4">
+                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
                         <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=33039&format=png&color=000000" alt=""/>
                         <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AWS</span>
                     </div>
