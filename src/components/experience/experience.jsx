e91c4cb2664dcc98bc4856cc88173bde81d630b5
Adair Sosa <jesussosa9859@gmail.com>
2025-09-23 22:39:21 -0600

    fix theme color dark

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -3,21 +3,21 @@ const school = () => {
         <>
             <div class='max-w-screen-xl flex flex-col lg:flex-row justify-center items-center lg:items-center p-5 pt-5 mx-auto gap-5'>
                 <div class="max-w-screen-xl flex flex-col items-center justify-center p-5 pt-5 mx-auto gap-1">
-                    <span class="text-2xl text-yellow-400 font-bold">Experience</span>
+                    <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Experience</span>
                 </div>
             </div>
             <div class='max-w-screen-xl flex flex-col justify-center items-center lg:items-center mx-auto  p-5 pt-5  gap-5'>
-                <div class="w-full items-start justify-center p-5 pt-5 border border-yellow-400 rounded-lg">
-                    <span class="text-2xl text-yellow-400 font-bold">Desarrollador BackEnd - Motivus</span> 
+                <div class="w-full items-start justify-center p-5 pt-5 border border-blue-600 dark:border-yellow-400 rounded-lg">
+                    <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Desarrollador BackEnd - Motivus</span> 
                 </div>
-                <div class="w-full items-start justify-center p-5 pt-5 border border-yellow-400 rounded-lg">
-                    <span class="text-2xl text-yellow-400 font-bold">Desarrollador Fulls Stack - KWN Software</span> 
+                <div class="w-full items-start justify-center p-5 pt-5 border border-blue-600 dark:border-yellow-400 rounded-lg">
+                    <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Desarrollador Fulls Stack - KWN Software</span> 
                 </div>
-                <div class="w-full items-start justify-center p-5 pt-5 border border-yellow-400 rounded-lg">
-                    <span class="text-2xl text-yellow-400 font-bold">Desarrollador BackEnd - Softitlan MX</span> 
+                <div class="w-full items-start justify-center p-5 pt-5 border border-blue-600 dark:border-yellow-400 rounded-lg">
+                    <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Desarrollador BackEnd - Softitlan MX</span> 
                 </div>
-                <div class="w-full items-start justify-center p-5 pt-5 border border-yellow-400 rounded-lg">
-                    <span class="text-2xl text-yellow-400 font-bold">Club de alumnos destacados - UATx</span> 
+                <div class="w-full items-start justify-center p-5 pt-5 border border-blue-600 dark:border-yellow-400 rounded-lg">
+                    <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Club de alumnos destacados - UATx</span> 
                 </div>
             </div>
         </>
