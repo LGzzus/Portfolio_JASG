d653ce271001796ab844028ee2d3398627a84e67
Adair Sosa <jesussosa9859@gmail.com>
2025-09-24 13:27:59 -0600

    fix icons and colors

--- src/components/school/school.jsx
+++ src/components/school/school.jsx
@@ -1,7 +1,40 @@
 const school = () => {
     return(
         <>
-            Aqui se vera la Escolaridad
+            <div class='max-w-screen-xl flex flex-col lg:flex-row justify-center items-center lg:items-center p-5 pt-5 mx-auto gap-5'>
+                <div class="max-w-screen-xl flex flex-col items-center justify-center p-5 pt-5 mx-auto gap-1">
+                    <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Escolaridad</span>
+                </div>
+            </div>
+            <div class='max-w-screen-xl flex flex-col justify-center items-center lg:items-center mx-auto  p-5 pt-5  gap-5'>
+                <div class="w-full items-start justify-center p-5 pt-5 border border-blue-600 dark:border-yellow-400 rounded-lg">
+                    <div class="flex flex-col gap-2">
+                        <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Maestria en DevOps</span>
+                        <span class="text-black dark:text-white">Universidad Internacional de la Rioja en Mexíco</span>
+                        <span class="text-black dark:text-white">Abril 2025 - Actualmente</span>
+                        <div class="max-w-screen flex flex-col lg:flex-row">
+                            <span class="text-black dark:text-white">
+                                Conocimientos en Herramientas de despliegues como lo es Ansible, Jenkins al igual que herramientas de IaC Terraform practicando con despliegues en contenedores Docker.
+                                Practicas en la nube de Aws y GCP conectando componentes nativos como Pub/Sub, Secret Manager, Router 53, EC2, Buckets, Cloud Run, GKE. 
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full items-start justify-center p-5 pt-5 border border-blue-600 dark:border-yellow-400 rounded-lg">
+                    
+                    <div class="flex flex-col gap-2">
+                        <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Licenciatura en Ingenieria en computación</span>
+                        <span class="text-black dark:text-white">Universidad Autonoma de Tlaxcala</span>
+                        <span class="text-black dark:text-white">2020 - Diciembre 2024</span>
+                        <div class="max-w-screen flex flex-col lg:flex-row">
+                            <span class="text-black dark:text-white">
+                                Especialización en sistemas distribuido, abarcando desde el analisis hasta la implantación, conocimiento en metodologías agiles, scrum, kanban, CMMI-dev.
+                                
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
--- src/components/technologies/technnologies.jsx
+++ src/components/technologies/technnologies.jsx
@@ -1,54 +1,57 @@
+import { DiAngularSimple } from 'react-icons/di';
 const tech = () => {
     return (
         <>
+        
            <div class='max-w-screen-xl flex flex-col items-center justify-center p-5 pt-5 mx-auto gap-1'>
                 <span class="text-2xl font-bold text-blue-600 dark:text-yellow-400">Tecnologias</span>
            </div>
             <div id="" class='max-w-screen-xl flex flex-col lg:flex-row justify-center items-center lg:items-center p-5 pt-5 mx-auto gap-5'>
                 <div class='grid  sm:grid-cols-1 md:grid-cols-2 md:grid col-end-3 gap-3 lg:grid-cols-4 justify-center'>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4 ">
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4 ">
                         <img class="h-auto max-w-auto rounded-lg" src="https://img.icons8.com/?size=100&id=90519&format=png&color=000000" alt=""/>
                         <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">Spring Boot</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=38561&format=png&color=000000" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PostgreSQL</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=36440&format=png&color=000000.
+                        " alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">PostgreSQL</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=cdYUlRaag9G9&format=png&color=000000" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Docker</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=22801&format=png&color=000000" alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">Docker</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=WHRLQdbEXQ16&format=png&color=000000" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GCP</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=20773&format=png&color=000000" alt=""/>
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">GCP</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=39858&format=png&color=0D5AA1" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MySQL</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=39858&format=png&color=000000" alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">MySQL</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=l9a5tcSnBwcf&format=png&color=F44336" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Angular</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=71256&format=png&color=000000" alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">Angular</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=GPfHz0SM85FX&format=png&color=000000" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Java</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=2572&format=png&color=000000" alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">Java</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=33039&format=png&color=000000" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AWS</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=al1VlNCK9eK5&format=png&color=000000" alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">AWS</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=8gWOBXY72Osj&format=png&color=000000" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">bash</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=10250&format=png&color=000000" alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">bash</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=8gWOBXY72Osj&format=png&color=000000" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Terraform</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=F2ZeQQjwdIJp&format=png&color=000000" alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">Terraform</span>
                     </div>
-                    <div class="flex bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
-                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=8gWOBXY72Osj&format=png&color=000000" alt=""/>
-                        <span class="text-blue-300 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Jenkins</span>
+                    <div class="flex gap-3 bg-blue-600 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-400 rounded-xl pr-4 pl-4">
+                        <img class="h-auto max-w-full rounded-lg" src="https://img.icons8.com/?size=100&id=41129&format=png&color=000000" alt=""/>
+                        <span class=" self-center text-2xl font-semibold whitespace-nowrap text-black">Jenkins</span>
                     </div>
                 </div>
             </div>
