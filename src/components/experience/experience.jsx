c86619484871941d4ec02c677a3114338459ba5f
Adair Sosa <jesussosa9859@gmail.com>
2025-09-23 23:34:31 -0600

    fix text section and colors

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -19,9 +19,7 @@ const school = () => {
                             en donde vivirian los distintos microservicios. 
                             Manejo y administro servicios nativos de la nube por mensionar algunos Pub/Sub, Api Gateway, Secret Manager, Buckets, Cloud SQl, Logging, Cloud Jobs, Cloud Run, entro otros.
                             </span>
-                            <div class="bg-black">
-                                <img class="h-auto max-w-auto rounded-lg" src={Motivus} alt=""/>
-                            </div>
+                            <img class="h-auto max-w-auto rounded-lg bg-black dark:bg-transparent" src={Motivus} alt=""/>
                         </div>
                     </div>
                 </div>
@@ -41,14 +39,23 @@ const school = () => {
                 <div class="w-full items-start justify-center p-5 pt-5 border border-blue-600 dark:border-yellow-400 rounded-lg">
                     <div class="flex flex-col">
                         <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Desarrollador BackEnd - Softitlan MX</span> 
-                        <span class="text-white">Fue parte de la guild de BackEnd en donde se desempeño como Desarrollador Java y  </span>
+                        <span class="text-black dark:text-white">
+                            Desarrollador FullStack Java Junior en el que participo en un proyecto para el cifrado de contraseñas
+                            para un gestor de contraseñas en el que se aplicaron algoritmos de cifrado y el uso de Secret Manager para la gestion de
+                            la clave privada que usa dicho algoritmo de cifrado. Tambien formo parte y fue lider de un equipo de desarrollo de una aplicación 
+                            Movil que ayudaba con la gestion de finanzas personales para este proyecto se consumieron librerias externas como [nombre libreria]
+                            que ayudaba a la elaboración de graficas
+                        </span>
                     </div>
                     
                 </div>
                 <div class="w-full items-start justify-center p-5 pt-5 border border-blue-600 dark:border-yellow-400 rounded-lg">
                     <div class="flex flex-col">
-                        <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Club de alumnos destacados - UATx</span> 
-                        <span class="text-white">Fue parte de la guild de BackEnd en donde se desempeño como Desarrollador Java y  </span>
+                        <span class="text-2xl text-blue-600 dark:text-yellow-400 font-bold">Club de programación de alumnos destacados - UATx</span> 
+                        <span class="text-black dark:text-white">
+                            Durante su recorrido escolar en la Universidad Autonóma de Tlaxcala pertenecio al Club de programación de alumnos destacados desarrollando pequeños proyectos
+                            para la institucion.
+                        </span>
                     </div>
                 </div>
             </div>
